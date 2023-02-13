8b5baf221faa735e77abf31757c9ecf0a7c70ce4
Vira Smulska <vera.smulskaya@gmail.com>
2023-02-13 20:53:25 +0100

    add datas as array

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+const movies = [{
+title: "Twilight",
+image: "https://cdn.shopify.com/s/files/1/0024/9803/5810/products/314678-Product-0-I_1920x.jpg?v=1572272185",
+description: "When Bella Swan moves to a small town in the Pacific Northwest, she falls in love with Edward Cullen, a mysterious classmate who reveals himself to be a 108-year-old vampire.",
+date: "2008",
+duration: "2 hours"
+}, {
+title: "Pride & Prejudice",
+image: "https://s12.stc.yc.kpcdn.net/share/i/12/6172202/de-1200.jpg",
+description: "Love develops between Elizabeth Bennet, a young woman of rank with no fortune, and Mr. Darcy, a handsome yet reserved man whose large estate is entailed upon his cousin. ",
+date: "2005",
+duration: "2 hours"
+}]
