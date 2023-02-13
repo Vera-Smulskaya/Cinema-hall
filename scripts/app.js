88462d3d561f00861abcd314c2f1a2dbe71cece5
Vira Smulska <vera.smulskaya@gmail.com>
2023-02-13 21:09:52 +0100

    add elements of DOM

--- scripts/app.js
+++ scripts/app.js
@@ -11,3 +11,44 @@ description: "Love develops between Elizabeth Bennet, a young woman of rank with
 date: "2005",
 duration: "2 hours"
 }]
+
+{/* <article class="card">
+        <header class="card__header">
+          <h2 class="card__title">Twilight</h2>
+          <span class="card__info">2008 - 2 hours</span>
+        </header>
+        <section class="card__content">
+          <p class="card__description">When Bella Swan moves to a small town in the Pacific Northwest, she falls in love with Edward Cullen, a mysterious classmate who reveals himself to be a 108-year-old vampire.</p>
+          <hr>
+          <p>Hellen Catherine Hardwicke</p>
+        </section>
+      </article> */}
+
+function createContent(movie) {
+const article = document.createElement("article");
+article.classList.add("card");
+
+const header = document.createElement("header");
+header.classList.add("card__header");
+header.style.backgroundImage = `url(${movie.image})`;
+
+const h2 = document.createElement("h2");
+h2.classList.add("card__title");
+h2.textContent = movie.title;
+
+const info = document.createElement("span");
+info.classList.add("card__info");
+info.textContent = `${movie.date} - ${movie.duration}`;
+
+const section = document.createElement("section");
+section.classList.add("card__content");
+
+const description = document.createElement("p");
+description.classList.add("card__description");
+description.textContent = movie.description;
+
+const line = document.createElement("hr");
+
+const director = document.createElement("p");
+director.textContent = movie.director;
+}
