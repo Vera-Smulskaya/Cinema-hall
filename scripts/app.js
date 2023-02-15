fa8ed48ae6a7d4e81b19e2eb1684e07fa9968781
Vira Smulska <vera.smulskaya@gmail.com>
2023-02-15 15:33:17 +0100

    rewrite logic using OOP principe

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ const movies = [
       "https://i.pinimg.com/550x/13/59/69/1359696e1122c35769af521a74ed0dcb.jpg",
     description:
       "When Bella Swan moves to a small town in the Pacific Northwest, she falls in love with Edward Cullen, a mysterious classmate who reveals himself to be a 108-year-old vampire.",
-    date: "2008",
+    date: "2008-12-05T00:00:00",
     duration: "2 hours",
     director: "Catherine Hardwicke",
   },
@@ -20,86 +20,70 @@ const movies = [
     director: "Joe Wright",
   },
 ];
+ 
+class Movie {
+  constructor(movie) {
+    this.title = movie.title;
+    this.image = movie.image;
+    this.description = movie.description;
+    this.date = new Date(movie.date).getFullYear;
+    this.director = movie.director;
+    this.duration = movie.duration;
+  }
 
-// function createContent(movie) {
-//   const article = document.createElement("article");
-//   article.classList.add("card");
+  createContent() {
+    const article = document.createElement("article");
+    article.classList.add("card");
+  
+    const header = document.createElement("header");
+    header.classList.add("card__header");
+    header.style.backgroundImage = `url(${this.image})`;
+  
+    const h2 = document.createElement("h2");
+    h2.classList.add("card__title");
+    h2.textContent = this.title;
+  
+    const info = document.createElement("span");
+    info.classList.add("card__info");
+    info.textContent = `${this.date} - ${this.duration}`;
+  
+    header.appendChild(h2);
+    header.appendChild(info);
+  
+    const section = document.createElement("section");
+    section.classList.add("card__content");
+  
+    const description = document.createElement("p");
+    description.classList.add("card__description");
+    description.textContent = this.description;
+  
+    const line = document.createElement("hr");
+  
+    const director = document.createElement("p");
+    director.textContent = this.director;
+  
+    section.appendChild(description);
+    section.appendChild(line);
+    section.appendChild(director);
+  
+    article.appendChild(header);
+    article.appendChild(section);
+  
+    return article;
+  }
+}
+class App {
+  static renderAll(movie) {
+    const el = document.getElementById("content");
+    const fragment = document.createDocumentFragment();
 
-//   const header = document.createElement("header");
-//   header.classList.add("card__header");
-//   header.style.backgroundImage = `url(${movie.image})`;
+    movies.forEach((movie) => {
+      fragment.appendChild(new Movie(movie).createContent());
+    });
 
-//   const h2 = document.createElement("h2");
-//   h2.classList.add("card__title");
-//   h2.textContent = movie.title;
-
-//   const info = document.createElement("span");
-//   info.classList.add("card__info");
-//   info.textContent = `${movie.date} - ${movie.duration}`;
-
-//   header.appendChild(h2);
-//   header.appendChild(info);
-
-//   const section = document.createElement("section");
-//   section.classList.add("card__content");
-
-//   const description = document.createElement("p");
-//   description.classList.add("card__description");
-//   description.textContent = movie.description;
-
-//   const line = document.createElement("hr");
-
-//   const director = document.createElement("p");
-//   director.textContent = movie.director;
-
-//   section.appendChild(description);
-//   section.appendChild(line);
-//   section.appendChild(director);
-
-//   article.appendChild(header);
-//   article.appendChild(section);
-
-//   return article;
-// }
-
-function createContentTemplate(movie) {
-  const article = `<article class="card">
-  <header class="card__header" style="background-image: url(${movie.image})">
-    <h2 class="card__title">${movie.title}</h2>
-    <span class="card__info">${movie.date} - ${movie.duration}</span>
-  </header>
-  <section class="card__content">
-    <p class="card__description">${movie.description}</p>
-    <hr>
-    <p>${movie.director}</p>
-  </section>
-</article> `;
-
-return createFragmentTemplate(article);
+    el.appendChild(fragment);
+  }
 }
 
-function createFragmentTemplate(str) {
-  const template = document.createElement("template");
+App.renderAll(movies);
 
-  template.innerHTML = str;
-
-  return template.content;
-}
-
-function appendContent(content) {
-  const el = document.getElementById("content");
-
-  el.appendChild(content);
-}
-
-function init() {
-  const fragment = document.createDocumentFragment();
-
-  movies.forEach((movie) => {
-    fragment.appendChild(createContentTemplate(movie));
-  });
-
-  appendContent(fragment);
-}
-
-init(movies);
