0c75e34bfd13a2c8a648775f3dc547b59a5d7fa9
Vira Smulska <vera.smulskaya@gmail.com>
2023-02-15 19:03:20 +0100

    add logic of sorting to js

--- scripts/app.js
+++ scripts/app.js
@@ -20,70 +20,55 @@ const movies = [
     director: "Joe Wright",
   },
 ];
- 
-class Movie {
-  constructor(movie) {
-    this.title = movie.title;
-    this.image = movie.image;
-    this.description = movie.description;
-    this.date = new Date(movie.date).getFullYear;
-    this.director = movie.director;
-    this.duration = movie.duration;
-  }
 
-  createContent() {
-    const article = document.createElement("article");
-    article.classList.add("card");
-  
-    const header = document.createElement("header");
-    header.classList.add("card__header");
-    header.style.backgroundImage = `url(${this.image})`;
-  
-    const h2 = document.createElement("h2");
-    h2.classList.add("card__title");
-    h2.textContent = this.title;
-  
-    const info = document.createElement("span");
-    info.classList.add("card__info");
-    info.textContent = `${this.date} - ${this.duration}`;
-  
-    header.appendChild(h2);
-    header.appendChild(info);
-  
-    const section = document.createElement("section");
-    section.classList.add("card__content");
-  
-    const description = document.createElement("p");
-    description.classList.add("card__description");
-    description.textContent = this.description;
-  
-    const line = document.createElement("hr");
-  
-    const director = document.createElement("p");
-    director.textContent = this.director;
-  
-    section.appendChild(description);
-    section.appendChild(line);
-    section.appendChild(director);
-  
-    article.appendChild(header);
-    article.appendChild(section);
-  
-    return article;
-  }
-}
-class App {
-  static renderAll(movie) {
-    const el = document.getElementById("content");
-    const fragment = document.createDocumentFragment();
+function createSorting() {
+  const nav = `
+    <a class="sort__item" href="${location.origin + location.pathname + '?sort=name'}">Name</a>
+    <a class="sort__item" href="${location.origin + location.pathname + '?sort=date'}">Date</a>
+  `;
 
-    movies.forEach((movie) => {
-      fragment.appendChild(new Movie(movie).createContent());
-    });
+  return createFragmentTemplate(nav);
+} 
 
-    el.appendChild(fragment);
-  }
+function createContentTemplate(movie) {
+  const article = `<article class="card">
+  <header class="card__header" style="background-image: url(${movie.image})">
+    <h2 class="card__title">${movie.title}</h2>
+    <span class="card__info">${movie.date} - ${movie.duration}</span>
+  </header>
+  <section class="card__content">
+    <p class="card__description">${movie.description}</p>
+    <hr>
+    <p>${movie.director}</p>
+  </section>
+</article> `;
+
+return createFragmentTemplate(article);
 }
 
-App.renderAll(movies);
+function createFragmentTemplate(str) {
+  const template = document.createElement("template");
 
+  template.innerHTML = str;
+
+  return template.content;
+}
+
+function appendContent(id, content) {
+  const el = document.getElementById(id);
+
+  el.appendChild(content);
+}
+
+function init() {
+  const fragment = document.createDocumentFragment();
+
+  movies.forEach((movie) => {
+    fragment.appendChild(createContentTemplate(movie));
+  });
+
+  appendContent("content", fragment);
+  appendContent("sort", createSorting());
+}
+
+init(movies);
